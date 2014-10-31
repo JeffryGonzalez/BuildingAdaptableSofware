926faf6f22de62e087f857c4c919b239f4da6352
Jeffry Gonzalez <jeff@hypertheory.com>
2014-10-31 11:23:33 -0400

    Added Delegates Solution

---
+++ solutions/RefactoringToDelegates/Tests/Conversions.cs
@@ -0,0 +1,17 @@
+﻿using ConvertersDomain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+	[TestClass]
+	public class Conversions
+	{
+		protected ImperialUnitsToMetricConverter _converter;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			_converter = new ImperialUnitsToMetricConverter();
+		}
+	}
+}
